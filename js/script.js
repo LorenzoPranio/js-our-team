f87c83cf5ace657ed1c8823d5d6c3d3a534d56e4
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-10 15:39:24 +0200

    [UPD]Added scaffolding for MILESTONE 0 in script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//MILESTONE 0
+//Struttura dati: Our Team
+
+const our_team = [
+    
+    //Ogni membro del team deve avere i seguenti dati: Nome, Ruolo, Foto
+    {
+        name: '',
+        role: '',
+        img: '',
+    },
+    {
+        name: '',
+        role: '',
+        img: '',
+    },
+    {
+        name: '',
+        role: '',
+        img: '',
+    },
+    {
+        name: '',
+        role: '',
+        img: '',
+    },
+    {
+        name: '',
+        role: '',
+        img: '',
+    },
+]
