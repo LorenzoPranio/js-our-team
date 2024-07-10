80d5dbdf02677b0c3718d20b4df31d1d9b5c708f
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-10 15:54:07 +0200

    [UPD]Added name, role and img in script.js

--- js/script.js
+++ js/script.js
@@ -2,31 +2,36 @@
 //Struttura dati: Our Team
 
 const our_team = [
-    
+
     //Ogni membro del team deve avere i seguenti dati: Nome, Ruolo, Foto
     {
-        name: '',
-        role: '',
-        img: '',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg',
     },
     {
-        name: '',
-        role: '',
-        img: '',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg',
     },
     {
-        name: '',
-        role: '',
-        img: '',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg',
     },
     {
-        name: '',
-        role: '',
-        img: '',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg',
     },
     {
-        name: '',
-        role: '',
-        img: '',
+        name: 'Scott Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg',
     },
 ]
